a74ed66458c12574b6cff44beace8366a5da1c10
greg <greg4253058@gmail.com>
2021-07-08 22:09:44 +0800

    加入專案檔案。

---
+++ WebApplication1/Hub/MonitorHub.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public interface IChatClient
+    {
+        /// <summary>
+        /// SignalR接收信息
+        /// </summary>
+        /// <param name="message">信息内容</param>
+        /// <returns></returns>
+        Task ReceiveMessage(object message);
+
+        /// <summary>
+        /// SignalR接收信息
+        /// </summary>
+        /// <param name="user">指定接收客户端</param>
+        /// <param name="message">信息内容</param>
+        /// <returns></returns>
+        Task ReceiveMessage(string user, string message);
+
+        Task ReceiveUpdate(object message);
+
+        Task ReceiveMachineData(string machineId, string message);
+        Task ReceiveMachine(string machineList);
+        Task GetRequestMachineData(string userId);
+    }
+
+    public class MonitorHub : Hub<IChatClient>
+    {
+
+        /// <summary>
+        /// 向指定群组发送信息
+        /// </summary>
+        /// <param name="groupName">组名</param>
+        /// <param name="message">信息内容</param>
+        /// <returns></returns>
+        public async Task SendMessageToGroupAsync(string groupName, string message)
+        {
+            await Clients.Group(groupName).ReceiveMessage(message);
+        }
+
+        /// <summary>
+        /// 加入指定组
+        /// </summary>
+        /// <param name="groupName">组名</param>
+        /// <returns></returns>
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        /// <summary>
+        /// 退出指定组
+        /// </summary>
+        /// <param name="groupName">组名</param>
+        /// <returns></returns>
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        /// <summary>
+        /// 向指定成员发送信息
+        /// </summary>
+        /// <param name="user">成员名</param>
+        /// <param name="message">信息内容</param>
+        /// <returns></returns>
+        public async Task SendPrivateMessage(string user, string message)
+        {
+            await Clients.User(user).ReceiveMessage(message);
+        }
+
+        /// <summary>
+        /// 当连接建立时运行
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnConnectedAsync()
+        {
+            //TODO..
+            return base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// 当链接断开时运行
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public override Task OnDisconnectedAsync(System.Exception ex)
+        {
+            //TODO..
+            return base.OnDisconnectedAsync(ex);
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.ReceiveMessage(user, message);
+        }
+
+        
+        public async Task WinFormsApp(string name, string msg)
+        {
+            await Clients.All.ReceiveUpdate($"接收到{name}的消息,消息内容：{msg}");
+        }
+        public async Task SendMachine(string sendMachineData)
+        {
+            await Clients.All.ReceiveMachine(sendMachineData);
+        }
+        public async Task SendMachineData(string machineId, string sendMachineData)
+        {
+            await Clients.All.ReceiveMachineData(machineId,sendMachineData);
+        }
+        public async Task AddToDataSource()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "DataSource");
+        }
+        
+        public async Task RequestMachineData()
+        {
+            await Clients.Group("DataSource").GetRequestMachineData(Context.ConnectionId);
+        }
+        public async Task SendUserMachine(string user, string sendMachine)
+        {
+            await Clients.Client(user).ReceiveMachine(sendMachine);
+        }
+
+    }
+
+
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalRChat.Hubs;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        ///// <summary>
+        ///// 中间件
+        ///// </summary>
+        //public class SignalRSendMildd
+        //{
+        //    /// <summary>
+        //    ///上下文
+        //    /// </summary>
+        //    private readonly RequestDelegate _next;
+
+        //    private readonly IHubContext<MonitorHub> _hubContext;
+
+        //    /// <summary>
+        //    ///注入上下文
+        //    /// </summary>
+        //    /// <param name="next"></param>
+        //    /// <param name="hubContext"></param>
+        //    public SignalRSendMildd(RequestDelegate next, IHubContext<MonitorHub> hubContext)
+        //    {
+        //        _next = next;
+        //        _hubContext = hubContext;
+        //    }
+
+        //    /// <summary>
+        //    /// SignalR过滤
+        //    /// </summary>
+        //    /// <param name="context"></param>
+        //    /// <returns></returns>
+        //    public async Task InvokeAsync(HttpContext context)
+        //    {
+        //        await _next(context);
+        //    }
+        //}
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSignalR();
+        }
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("AllowCors");
+            //app.UseSignalRSendMildd();
+            //app.UseSignalR(routes =>
+            //{
+            //    routes.MapHub<ChatHub>("/chatHub");
+            //    routes.MapHub<MonitorHub>("/MonitorHub");
+            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<MonitorHub>("/MonitorHub");
+                endpoints.MapHub<ChatHub>("/chatHub");
+            });
+        }
+    }
+}
